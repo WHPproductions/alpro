a6ad255ebffaec7ba896778054e4cea772563f36
WHPproductions <139813100+WHPproductions@users.noreply.github.com>
2025-07-13 17:09:18 +0700

    sadasdasdasdasd

---
+++ src/main/java/com/mycompany/alpro_ui/Alpro_UI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.alpro_ui;
+
+/**
+ *
+ * @author PC Keluarga
+ */
+public class Alpro_UI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
